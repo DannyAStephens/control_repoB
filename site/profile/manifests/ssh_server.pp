eb298ff779a5475407c1d827f63deb0a6567914d
Danny Stephens <danny.a.stephens@gmail.com>
2018-06-04 10:31:19 -0500

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,16 @@
+class profile::ssh_server {
+  package {'openssh-server':
+      ensure => present,
+  }
+  service {'sshd':
+      ensure => 'running',
+      enable => 'true',
+  }
+  ssh_authorized_key {'root@master.puppet.vm':
+      ensure => present,
+      user   => 'root'
+      type   => 'ssh-rsa',
+      key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJr32fAzas69pmlRJ+/v23jnC3iu8CXbv6+wUbeaPJRH3n2F9vipdM/X9iuOXzglvT+TSJhgyakXYbST2726sBsIUQJBkKJcorJ2V+WlpNJiI3vwW7KVxes/YKk7qgWi0o8vQdLfjdWjw3feaOU4jrRr1UNxND3Parvt/VONpKaUVmL+/Q0rF3qjhkuZ7GCxYzRlnGtka1MFVvTdDnYFwc7vWrT0P1DSIAVqzvnWBO+PiAz1Xp2tGO1wf2oDzXqDlPnYNuty1BaPg7HYOEe0qi3O48ZZ187JMofMoooCYEVVWFFSQi4aUaXFhSARWUZdULksKJG2QaEcly2tuVTsNT',
+  }
+}
+   
